2768d4c8290dc468a2bbc4aca749192fd72f7bcd
Martin Aspeli <optilude@gmail.com>
2018-06-10 22:19:52 +0100

    Add tests for month / days breakdown

--- jira_agile_metrics/utils_test.py
+++ jira_agile_metrics/utils_test.py
@@ -8,6 +8,7 @@ from .utils import (
     to_days_since_epoch,
     extend_dict,
     breakdown_by_month,
+    breakdown_by_month_sum_days,
     to_bin
 )
 
@@ -135,6 +136,116 @@ def test_breakdown_by_month_none_values():
         pd.Timestamp(2018, 3, 1),
     ]
     assert breakdown.to_dict('records') == [{None: 3}, {None: 4}, {None: 4}]
+
+def test_breakdown_by_month_sum_days():
+
+    df = pd.DataFrame([
+        {'priority': 'high', 'start': pd.Timestamp(2018, 1, 1), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 31 Feb: 28 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 2), 'end': pd.Timestamp(2018, 1, 20)},  # Jan: 19 Feb:  0 Mar:  0
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 3), 'end': pd.Timestamp(2018, 3, 20)},  # Jan:  0 Feb: 26 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 4), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 28 Feb: 28 Mar: 20
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 5), 'end': pd.Timestamp(2018, 2, 20)},  # Jan:  0 Feb: 16 Mar:  0
+        {'priority': 'med',  'start': pd.Timestamp(2018, 3, 6), 'end': pd.Timestamp(2018, 3, 20)}   # Jan:  0 Feb:  0 Mar: 15
+    ], columns=['key', 'priority', 'start', 'end'])
+
+    breakdown = breakdown_by_month_sum_days(df, 'start', 'end', 'priority', ['low', 'med', 'high'])
+    assert list(breakdown.columns) == ['med', 'high']
+    
+    assert list(breakdown.index) == [
+        pd.Timestamp(2018, 1, 1),
+        pd.Timestamp(2018, 2, 1),
+        pd.Timestamp(2018, 3, 1),
+    ]
+
+    assert breakdown.to_dict('records') == [
+        {'high': 31.0, 'med': 47.0},
+        {'high': 70.0, 'med': 28.0},
+        {'high': 40.0, 'med': 35.0}
+    ]
+
+def test_breakdown_by_month_sum_days_no_column_spec():
+
+    df = pd.DataFrame([
+        {'priority': 'high', 'start': pd.Timestamp(2018, 1, 1), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 31 Feb: 28 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 2), 'end': pd.Timestamp(2018, 1, 20)},  # Jan: 19 Feb:  0 Mar:  0
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 3), 'end': pd.Timestamp(2018, 3, 20)},  # Jan:  0 Feb: 26 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 4), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 28 Feb: 28 Mar: 20
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 5), 'end': pd.Timestamp(2018, 2, 20)},  # Jan:  0 Feb: 16 Mar:  0
+        {'priority': 'med',  'start': pd.Timestamp(2018, 3, 6), 'end': pd.Timestamp(2018, 3, 20)}   # Jan:  0 Feb:  0 Mar: 15
+    ], columns=['key', 'priority', 'start', 'end'])
+
+    breakdown = breakdown_by_month_sum_days(df, 'start', 'end', 'priority')
+    assert list(breakdown.columns) == ['high', 'med']  # alphabetical
+    
+    assert list(breakdown.index) == [
+        pd.Timestamp(2018, 1, 1),
+        pd.Timestamp(2018, 2, 1),
+        pd.Timestamp(2018, 3, 1),
+    ]
+
+    assert breakdown.to_dict('records') == [
+        {'high': 31.0, 'med': 47.0},
+        {'high': 70.0, 'med': 28.0},
+        {'high': 40.0, 'med': 35.0}
+    ]
+
+def test_breakdown_by_month_sum_day_open_ended():
+
+    df = pd.DataFrame([
+        {'priority': 'high', 'start': pd.Timestamp(2018, 1, 1), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 31 Feb: 28 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 2), 'end': pd.Timestamp(2018, 1, 20)},  # Jan: 19 Feb:  0 Mar:  0
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 3), 'end': pd.Timestamp(2018, 3, 20)},  # Jan:  0 Feb: 26 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 4), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 28 Feb: 28 Mar: 20
+        {'priority': 'high', 'start': pd.Timestamp(2018, 2, 5), 'end': pd.Timestamp(2018, 2, 20)},  # Jan:  0 Feb: 16 Mar:  0
+        {'priority': 'med',  'start': pd.Timestamp(2018, 3, 6), 'end': None}                        # Jan:  0 Feb:  0 Mar: 26
+    ], columns=['key', 'priority', 'start', 'end'])
+
+    breakdown = breakdown_by_month_sum_days(df, 'start', 'end', 'priority', ['low', 'med', 'high'])
+    assert list(breakdown.columns) == ['med', 'high']
+
+    # Note: We will get columns until the current month; assume this test is
+    # run from June onwards ;)
+    
+    assert list(breakdown.index)[:5] == [
+        pd.Timestamp(2018, 1, 1),
+        pd.Timestamp(2018, 2, 1),
+        pd.Timestamp(2018, 3, 1),
+        pd.Timestamp(2018, 4, 1),
+        pd.Timestamp(2018, 5, 1),
+    ]
+    assert breakdown.to_dict('records')[:5] == [
+        {'high': 31.0, 'med': 47.0},
+        {'high': 70.0, 'med': 28.0},
+        {'high': 40.0, 'med': 46.0},
+        {'high': 0,    'med': 30.0},
+        {'high': 0,    'med': 31.0},
+    ]
+
+def test_breakdown_by_month_sum_days_none_values():
+
+    df = pd.DataFrame([
+        {'priority': None,   'start': pd.Timestamp(2018, 1, 1), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 31 Feb: 28 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 2), 'end': pd.Timestamp(2018, 1, 20)},  # Jan: 19 Feb:  0 Mar:  0
+        {'priority': None,   'start': pd.Timestamp(2018, 2, 3), 'end': pd.Timestamp(2018, 3, 20)},  # Jan:  0 Feb: 26 Mar: 20
+        {'priority': 'med',  'start': pd.Timestamp(2018, 1, 4), 'end': pd.Timestamp(2018, 3, 20)},  # Jan: 28 Feb: 28 Mar: 20
+        {'priority': None,   'start': pd.Timestamp(2018, 2, 5), 'end': pd.Timestamp(2018, 2, 20)},  # Jan:  0 Feb: 16 Mar:  0
+        {'priority': 'med',  'start': pd.Timestamp(2018, 3, 6), 'end': pd.Timestamp(2018, 3, 20)}   # Jan:  0 Feb:  0 Mar: 15
+    ], columns=['key', 'priority', 'start', 'end'])
+
+    breakdown = breakdown_by_month_sum_days(df, 'start', 'end', 'priority')
+    assert list(breakdown.columns) == [None, 'med']
+    
+    assert list(breakdown.index) == [
+        pd.Timestamp(2018, 1, 1),
+        pd.Timestamp(2018, 2, 1),
+        pd.Timestamp(2018, 3, 1),
+    ]
+
+    assert breakdown.to_dict('records') == [
+        {None: 31.0, 'med': 47.0},
+        {None: 70.0, 'med': 28.0},
+        {None: 40.0, 'med': 35.0}
+    ]
 
 def test_to_bin():
 
