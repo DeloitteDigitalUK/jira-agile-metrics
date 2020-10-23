dc5adb3258b1a3e30e599674306134a609583177
Dmytro Lapshyn <dmytro.lapshyn@sigmaukraine.com>
2020-10-23 12:32:28 +0300

    Fixing typo in the test data in querymanager test

--- jira_agile_metrics/querymanager_test.py
+++ jira_agile_metrics/querymanager_test.py
@@ -21,7 +21,7 @@ def jira(custom_fields):
                 "A-1",
                 summary="Issue A-1",
                 issuetype=Value("Story", "story"),
-                status=Value("Backlotg", "backlog"),
+                status=Value("Backlog", "backlog"),
                 resolution=None,
                 created="2018-01-01 01:01:01",
                 customfield_001="Team 1",
